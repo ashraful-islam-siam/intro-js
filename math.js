afd6b73b2585d05c3e72f5a460693e7f17a74132
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-08-28 12:47:47 +0600

    learning math

---
+++ math.js
@@ -0,0 +1,50 @@
+// যোগ করতে চাইলে ( + ) সাইন ব্যবহার করবো
+
+// উদাহরণ
+console.log("_____যোগ_____");
+console.log(5 + 2); // 7
+console.log(10 + 7 + 25); // 42
+console.log(10 + 9 + 11 + 25); // 55
+console.log(80 + 62 + 36 + 41 + 511); // 730
+
+let a = 5;
+let b = 10;
+let x = 25;
+let y = 98;
+console.log(a + b); // 15
+console.log(a + b + x + y); // 138
+
+// বিয়োগ করতে চাইলে ( - ) সাইন ব্যবহার করবো
+
+// উদাহরণ
+console.log("_____বিয়োগ_____");
+console.log(5 - 2); // 3
+console.log(25 - 10 - 3); // 12
+console.log(98 - 50 - 25 - 10); // 13
+console.log(511 - 41 - 36 - 15 - 10 - 2); // 407
+
+let j = 5 - 2; // 3
+let h = 25 - 10 - 3; // 12
+let k = 98 - 50 - 25 - 10; // 13
+let l = 511 - 41 - 36 - 15 - 10 - 2; // 407
+console.log(j);
+console.log(h);
+console.log(k);
+console.log(l);
+
+// গুন করতে চাইলে ( * ) সাইন ব্যবহার করবো
+
+// উদাহরণ
+console.log("_____গুন_____");
+console.log(10 * 2);
+console.log(10 * 2 * 3);
+console.log(10 * 3 * 9 * 4);
+
+let aa = 10 * 7;
+let bb = 20 * 1 * 3;
+let cc = 9 * 2 * 4;
+let dd = 8 * 7 * 2 * 3;
+console.log(aa);
+console.log(bb);
+console.log(cc);
+console.log(dd);
