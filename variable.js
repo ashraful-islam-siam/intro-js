88b1e9e09fab8da58147baa4e86cb89cb09e78a1
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-08-28 12:14:10 +0600

    variable updated

--- variable.js
+++ variable.js
@@ -1,5 +1,29 @@
+// String - কোনো ক্যারেক্টার লেখতে আমরা স্ট্রিং ব্যবহার করি
 let firstName = "Ashraful";
 let lastName = "Islam";
 let fullName = firstName + " " + lastName;
 
 console.log("Assalamuwalaikum," + " " + fullName);
+
+// Number - কোনো নাম্বার নিতে চাইলে আমরা নাম্বার ডেটা টাইপ ব্যবহার করবো
+let banana = 5;
+let apple = 25;
+let orange = 15;
+
+console.log(banana);
+console.log(apple);
+console.log(orange);
+
+// Boolean - বুলিয়ান সত্য বা মিথ্যা হবে।
+let isExam = true;
+let isLearner = true;
+let isGirl = false;
+console.log(isExam);
+console.log(isLearner);
+console.log(isGirl);
+
+// Undefined - কোনো ভেরিএবল ডিক্লার করার পড়ে যদি ভেরিএবলে ভেলু না দেয় তাহলে সেটা undefined
+let idNumber = undefined;
+let age;
+console.log(idNumber);
+console.log(age);
